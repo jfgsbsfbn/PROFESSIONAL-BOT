1d36a1eceb5b2e5587eeaa4db150b975f8d241bc
legend-boker <39490532+legend-boker@users.noreply.github.com>
2018-07-01 22:01:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -825,6 +825,12 @@ client.on('messageDelete', message => {
 });
 
 client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send('``! KND | للديسكورد`` ``Ӄᴎ∂⍣㦵 | للعبة`` ')
+    }
+});
+
+client.on('message', message => {
     if(message.content === 'باك'){
         message.channel.send(' ** :) وُلُِڪم ي قٌلُِبَي منوُرٍ آلُِڪلُِآن بَرٍجٍعٍتڪ   ** [user] ')
     }
